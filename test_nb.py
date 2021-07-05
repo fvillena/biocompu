3b06d71b17f0993e183669cc53eab830f7e9d895
Fabián Villena <villenafabian@gmail.com>
2021-07-04 21:27:44 -0400

    test nb

---
+++ test_nb.py
@@ -0,0 +1,14 @@
+import subprocess
+import tempfile
+
+
+def _exec_notebook(path):
+    with tempfile.NamedTemporaryFile(suffix=".ipynb") as fout:
+        args = ["jupyter", "nbconvert", "--to", "notebook", "--execute",
+                "--ExecutePreprocessor.timeout=1000",
+                "--output", fout.name, path]
+        subprocess.check_call(args)
+
+
+def test_1():
+    _exec_notebook('1-clustering.ipynb')
