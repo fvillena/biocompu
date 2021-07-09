e12881fbfe36882e982aa21d3a17411e5e6c9367
Fabián Villena <villenafabian@gmail.com>
2021-07-09 15:29:50 -0400

    lesson 1

--- test_nb.py
+++ test_nb.py
@@ -10,5 +10,8 @@ def _exec_notebook(path):
         subprocess.check_call(args)
 
 
-def test_1():
-    _exec_notebook('1-clustering.ipynb')
+def test_1_1():
+    _exec_notebook('1.1-clustering.ipynb')
+
+def test_1_2():
+    _exec_notebook('1.2-dimensionality_reduction.ipynb')
